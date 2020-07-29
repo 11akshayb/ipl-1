80bf82b63ba47c6d7cde601e569939ea140bf0ef
AkshayB <11akshayb@users.noreply.github.com>
2020-07-29 16:13:22 +0530

    Vizualising Most Man of the Matches Each Season

--- public/app-extra-solution.js
+++ public/app-extra-solution.js
@@ -1,50 +1,100 @@
 function fetchAndVisualizeData (){
-    fetch("./ExtraSolution.json")
-    .then(res=>res.json())
-    .then(visualizeData);
+  fetch("./ExtraSolution.json")
+  .then(res=>res.json())
+  .then(visualizeData);
 }
 
 fetchAndVisualizeData()
 
 function visualizeData(data){
     visualizeTossesMatches(data.tossMatchesWon)
+    visualizeManOfMatchEachSeason(data.manOfMatchEachSeason)
     return;
 }
 
 function visualizeTossesMatches(tossesMatchesWon){
-    const visualizationArray = [];
-    for (let team in tossesMatchesWon)  {
-      visualizationArray.push([team, tossesMatchesWon[team]]);
-    }
-  
-    Highcharts.chart("tosses_and_Matches", {
-      chart: {
-        type: "column"
-      },
+  const visualizationArray = [];
+  for (let team in tossesMatchesWon)  {
+    visualizationArray.push([team, tossesMatchesWon[team]]);
+  }
+
+  Highcharts.chart("tosses_and_Matches", {
+    chart: {
+      type: "column"
+    },
+    title: {
+      text: "Tosses & Matches Won Per Team"
+    },
+    // subtitle: {
+    //   text:
+    //     'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+    // },
+    xAxis: {
       title: {
-        text: "Tosses & Matches Won Per Team"
+        text: "Teams"
       },
-      // subtitle: {
-      //   text:
-      //     'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
-      // },
-      xAxis: {
-        title: {
-          text: "Teams"
-        },
-        type: "category"
+      type: "category"
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: "Wins"
+      }
+    },
+    series: [
+      {
+        name: "Tosses and Matches Won",
+        data: visualizationArray
+      }
+    ]
+  });
+}
+
+
+function visualizeManOfMatchEachSeason(manOfMatchEachSeason){
+  var seasons = Object.keys(manOfMatchEachSeason)
+  var players = [];
+  for(let season in manOfMatchEachSeason){
+  players.push(Object.keys(manOfMatchEachSeason[season]))
+  }
+  var x = players.flat();
+
+  let visualizationArray=[];
+  visualizationArray = x.map(player => ({
+  name: player,
+  data: seasons.map(season => manOfMatchEachSeason[season][player] || 0)
+  }));
+
+  Highcharts.chart('man_of_match', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Highest Man of The Match every season'
+    },
+    subtitle: {
+      text:
+        'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+    },
+    xAxis: {
+      title: {
+        text: "Seasons"
       },
-      yAxis: {
+        categories: seasons,
+        crosshair: true
+    },
+    yAxis: {
         min: 0,
         title: {
-          text: "Wins"
+            text: 'Man of Matches'
         }
-      },
-      series: [
-        {
-          name: "Tosses and Matches Won",
-          data: visualizationArray
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.01,
+            borderWidth: 0
         }
-      ]
-    });
+    },
+    series: visualizationArray
+});
 }
